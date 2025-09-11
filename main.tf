4c20e9c8ae44c2d4b2ab60bda483b12d44c10a6e
Malleswari-padigapati <padigapatinagamalleswari@gmail.com>
2025-09-11 11:33:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,39 @@
 provider "aws" {
-  region = "ap-south-1"   # change to your AWS region
+  region = "ap-south-1"
 }
 
-resource "aws_instance" "web" {
-  ami           = "ami-02d26659fd82cf299"  
+resource "aws_instance" "docker_app" {
+  ami           = "ami-02d26659fd82cf299" 
   instance_type = "t2.micro"
-  key_name      = "practice"               
+  key_name      = "practice"              
 
-  vpc_security_group_ids = ["sg-0907581ea8223e15b"] # security group (allow 22 + 80)
+  vpc_security_group_ids = ["sg-0907581ea8223e15b"] 
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update -y
+              apt-get install -y docker.io git
+
+              # Start Docker
+              systemctl start docker
+              systemctl enable docker
+
+              # Clone GitHub repo
+              git clone https://github.com/Malleswari-padigapati/myproject.git /home/ubuntu/myproject
+
+              # Build Docker image
+              cd /home/ubuntu/myproject
+              docker build -t myapp:latest .
+
+              
+              docker run -d -p 8089:5000 myapp:latest
+              EOF
 
   tags = {
-    Name = "jenkins-docker-app"
+    Name = "jenkins-terraform-docker"
   }
 }
 
 output "public_ip" {
-  value = aws_instance.web.public_ip
+  value = aws_instance.docker_app.public_ip
 }
