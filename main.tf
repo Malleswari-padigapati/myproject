d350bf21cd0660b5c5d3e46f328826df29d0e60b
Malleswari-padigapati <padigapatinagamalleswari@gmail.com>
2025-09-12 13:24:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,51 +3,53 @@ provider "aws" {
 }
 
 resource "aws_instance" "docker_app" {
-  ami           = "ami-02d26659fd82cf299"
+  ami           = "ami-02d26659fd82cf299" # Ubuntu 22.04 LTS
   instance_type = "t2.micro"
-  key_name      = "practice"
-
-  vpc_security_group_ids = ["sg-0907581ea8223e15b"]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update -y
-              apt-get install -y docker.io git
-
-              # Start Docker
-              systemctl start docker
-              systemctl enable docker
-
-              # Clone GitHub repo (branch man)
-              git clone -b man https://github.com/Malleswari-padigapati/myproject.git /home/ubuntu/myproject
-
-              # Build Docker image
-              cd /home/ubuntu/myproject
-              docker build -t myapp:latest .
-
-              # Run container on port 80
-              docker run -d -p 80:5000 myapp:latest
-              EOF
+  key_name      = "practice"              # your key pair name
+  vpc_security_group_ids = ["sg-0907581ea8223e15b"] # must allow 22,80
 
   tags = {
     Name = "jenkins"
   }
 
-  # Run docker ps after creation
+  # Run all setup steps synchronously
   provisioner "remote-exec" {
     inline = [
-      "docker ps"
+      # Update & install dependencies
+      "sudo apt-get update -y",
+      "sudo apt-get install -y docker.io git",
+
+      # Start and enable Docker
+      "sudo systemctl start docker",
+      "sudo systemctl enable docker",
+
+      # Clone GitHub repo (branch man)
+      "git clone -b man https://github.com/Malleswari-padigapati/myproject.git /home/ubuntu/myproject || true",
+
+      
+      "cd /home/ubuntu/myproject && sudo docker build -t myapp:latest .",
+
+      
+      "sudo docker run -d -p 80:5000 myapp:latest",
+
+      
+      "sudo docker ps"
     ]
 
     connection {
       type        = "ssh"
       user        = "ubuntu"
-      private_key = file("practice.pem")
+      private_key = file("practice.pem") # path to your PEM file
       host        = self.public_ip
     }
   }
 }
 
+# Outputs
 output "public_ip" {
   value = aws_instance.docker_app.public_ip
+}
+
+output "app_url" {
+  value = "http://${aws_instance.docker_app.public_ip}"
 }
